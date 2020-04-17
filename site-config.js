b123bef3456284afc65b87031305d5b2dbcb5ad7
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2020-04-17 09:44:11 -0400

    first commit with files

---
+++ site-config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  // App Info
+  appstore_link         : "https://apps.apple.com/my/app/body-insights/id1397531585",                                  
+  playstore_link        : null,                                        
+  google_analytics_ID   : null,                                       
+  presskit_download_link: "https://drive.google.com/file/d/1GV8si_GU7w5yLz1aJHZQzPq5N67l2u_T/view?usp=sharing",                                        
+  video_or_screenshot   : "screenshot",                                 // "screenshot" or "video"
+  app_url               : "https://bodyinsights.app/",                              // Domain of your website without path_prefix.
+  path_prefix           : "/",                                          // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "Body Insights",
+  app_price             : null,
+  app_tagline           : "Your health, customized for you.",
+  app_keywords          : ["Health", "Steps", "Pedometer", "Walking", "Body"],
+
+  // Personal Info
+  your_name              : "Ricky Witherspoon ",
+  your_link              : "https://rsw3.xyz",
+  your_city              : "New Hampshire",
+  email_address          : "richardwitherspoon3@gmail.com",
+  linkedin_username      : "Rspoon3",
+  facebook_username      : null,
+  instagram_username     : "Rspoon3",
+  twitter_username       : "Rspoon3",
+  github_username        : "Rspoon3",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "Simple",
+      description: "A beautiful, clean interface makes it easy to find and understand your health metrics.",
+      fontawesome_icon_name: "star",
+    },
+    {
+      title: "Customizable",
+      description: "See bar charts or line graphs along with statistics of the most popular categories supported by iOS.",
+      fontawesome_icon_name: "cogs",
+    },
+    {
+      title: "Goal Oriented",
+      description: "Keep track of what's important to you by setting your own goals, and earn achievements by staying active.",
+      fontawesome_icon_name: "award",
+    },
+  ],
+  header_background             : "rgba(0, 0, 0, 0.2)",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "#2d385480",
+  device_color                  : "blue",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "#001929",
+  primary_text_color            : "#000",
+  content_width                 : "1170px",
+  font                          : `"Helvetica Neue", sans-serif`,
+  heading_color                 : "#3587f7",
+  link_color                    : "#3587f7",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_tagline_color             : "#ffffff",
+  feature_title_color           : "#ffffff",
+  feature_text_color            : "#999999",
+  feature_icons_foreground_color: "#3587f7",
+  feature_icons_background_color: "#f6f6f6",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#e6e6e6",
+  footer_text_color             : "#888888",
+
+  //7ed0fb light blue hear
+  //3587f7 dark blue heart
+}
